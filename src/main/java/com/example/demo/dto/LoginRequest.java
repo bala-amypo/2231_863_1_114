a5b6cbbb8799a2526d9297d13318cc832aa946b4
bala-amypo <bala@amypo.in>
2025-12-21 20:37:41 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoginRequest.java
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -1,8 +1,12 @@
 package com.yourpackage.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data // Use Lombok to generate getters/setters automatically
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
