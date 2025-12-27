f75f3ed85bdd27efc319cdc615494e66c7a9cbd8
bala-amypo <bala@amypo.in>
2025-12-27 04:54:39 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,38 +1,4 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-            // Disable CSRF (important for Swagger & REST)
-            .csrf(csrf -> csrf.disable())
-
-            // Allow everything
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                    "/swagger-ui.html",
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**",
-                    "/h2-console/**",
-                    "/**"
-                ).permitAll()
-                .anyRequest().permitAll()
-            )
-
-            // Disable default login page
-            .formLogin(form -> form.disable())
-
-            // Disable HTTP basic auth popup
-            .httpBasic(basic -> basic.disable());
-
-        return http.build();
-    }
 }
