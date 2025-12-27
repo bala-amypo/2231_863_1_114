425f196e234adc8608f05006641e605a13de0098
bala-amypo <bala@amypo.in>
2025-12-27 05:18:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/ServiceCounter.java
+++ src/main/java/com/example/demo/entity/ServiceCounter.java
@@ -1,10 +1,22 @@
-package com.example.demo.entity;
-
 public class ServiceCounter {
+
     private Long id;
     private String counterName;
-    private String department;
-    private Boolean isActive = true;
+    private Boolean isActive;
 
-    // getters & setters
+    public Boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getCounterName() {
+        return counterName;
+    }
+
+    public void setCounterName(String counterName) {
+        this.counterName = counterName;
+    }
 }
