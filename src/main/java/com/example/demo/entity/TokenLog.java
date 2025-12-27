9b65e842e5b20f4a6f3d3828988c34fb6d0ec5eb
bala-amypo <bala@amypo.in>
2025-12-27 05:39:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/TokenLog.java
+++ src/main/java/com/example/demo/entity/TokenLog.java
@@ -1,17 +1,61 @@
 package com.example.demo.entity;
 
-public class TokenLog {
+import jakarta.persistence.*;
 
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
     private Long id;
-    private String message;
-    private Token token;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    private String name;
 
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+    @Column(unique = true)
+    private String email;
 
-    public Token getToken() { return token; }
-    public void setToken(Token token) { this.token = token; }
+    private String password;
+
+    private String role = "STAFF";
+
+    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
